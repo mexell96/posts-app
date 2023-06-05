a8653481608dff25ff46b895b157168f8a0eb4cd
artem_zagovelev <mexell96@gmail.com>
2023-06-05 13:40:56 +0300

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-const App = () => {
-  return (
-    <div>
-      <p>Hello</p>
-    </div>
-  );
-};
+import { Routes, Route } from "react-router-dom";
+
+import Posts from "./pages/Posts";
+import About from "./pages/About";
+import User from "./pages/User";
+
+const App = () => (
+  <Routes>
+    <Route path="/" element={<Posts />} />
+    <Route path="/:page" element={<Posts />} />
+    <Route path="/about" element={<About />} />
+    <Route path="/users/:id" element={<User />} />
+  </Routes>
+);
 
 export default App;
